68bf2cbccb59a30bdca4551d3059df3f5f8989c9
Natalia723 <dpardo@redsis.com>
2025-02-06 15:18:10 -0500

    Base structrure project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
